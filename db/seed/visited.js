4cbb109725a54e142a1cfaa0e864a51c89a06d73
Noa <noa.benaziz@gmail.com>
2022-04-08 12:26:47 +0200

    user added visited file

--- db/seed/visited.js
+++ db/seed/visited.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose')
 const Country = require('../../models/Country.model')
 const Visited = require('../../models/Visited.model')
+const User = require('../../models/User.model')
 
 const connection = require('../index')
 
 const createdVisited = async () => {
   const countries = await Country.find().select('_id')
+  const user = await User.findOne({username: 'Noa'})
   const visited = await Visited.create({
     country: countries[Math.floor(Math.random() * countries.length)],
+    user: user
   })
 }
+
 
 for (let i = 0; i < 20; i++) {
   createdVisited()
