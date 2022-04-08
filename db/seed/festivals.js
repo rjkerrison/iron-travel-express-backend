af1cf7ea0f7c8c03032a501e840fd416c8ec8018
Léo Cheffi <contact@leocheffi.com>
2022-04-08 12:08:11 +0200

    done

--- db/seed/festivals.js
+++ db/seed/festivals.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
 const Festival = require('../../models/Festivals.model')
+const festivalJson = require('./festivals.json')
+const Country = require('../../models/Country.model')
+require('../index')
 
 const createFestival = async (req, res, next) => {
-  await Festival.findOne()
-  await Festival.create()
-  return
+  for (const festivalToAdd of festivalJson) {
+    const country = await Country.findOne({ cca3: festivalToAdd.cca3 })
+    if (country) {
+      await Festival.create({ ...festivalToAdd, country: country._id })
+    }
+  }
 }
+createFestival()
