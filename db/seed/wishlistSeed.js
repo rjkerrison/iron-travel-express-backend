34feab6927b40834d23598ac6f67c989b3972075
potapen <po@ta.pen>
2022-04-08 14:06:51 +0200

    done

--- db/seed/wishlistSeed.js
+++ db/seed/wishlistSeed.js
@@ -5,25 +5,38 @@ const connection = require('../index')
 const { default: mongoose } = require('mongoose')
 
 const CreateUser = async () => {
-    await User.create({
-        username: 'Noa',
-        password: 'azertyuiop'
-    })
+  await User.create({
+    username: 'Noa',
+    password: 'azertyuiop',
+  })
 }
 
 const seedWishlist = async () => {
-    const user = await User.findOne({username: 'Noa'})
-    const countryFav = await Country.findOne({cc3: 'ESP'})
-    await Wishlist.create({country: countryFav, user: user})
+  const user = await User.findOne({ username: 'Noa' })
+  const countryList = ['JPN', 'FRA']
+  // countryList.forEach(async (country) => {
+  //   const countryFav = await Country.find({ cca3: country })
+  //   await Wishlist.create({ country: countryFav, user: user })
+  // })
+  for (let i = 0; i < countryList.length; i++) {
+    console.log('countryList[i]', countryList[i])
+    const countryFav = await Country.findOne({ cca3: countryList[i] })
+    console.log('countryFav', countryFav)
+    console.log('countryFav._id', countryFav._id)
+    const createdWishlist = await Wishlist.create({
+      country: countryFav,
+      user: user,
+    })
+    console.log('createdWishlist', createdWishlist)
+  }
 }
 
 const perform = async () => {
-    await connection
-  
-    await CreateUser()
-    await seedWishlist()
-  
-    await mongoose.connection.close()
+  await connection
+  await CreateUser()
+  await seedWishlist()
+
+  await mongoose.connection.close()
 }
 
 perform()
