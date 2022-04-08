c2b540324375017e3d3d3ddbbb89d92edb92bf40
Léo Cheffi <contact@leocheffi.com>
2022-04-08 10:48:37 +0200

    add routes and festival model

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const authRoutes = require('./auth.routes')
 const countryRoutes = require('./country.routes')
+const festivalRoutes = require('./festival.routes')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -9,5 +10,6 @@ router.get('/', (req, res, next) => {
 
 router.use('/auth', authRoutes)
 router.use('/countries', countryRoutes)
+router.use('/festival', festivalRoutes)
 
 module.exports = router
