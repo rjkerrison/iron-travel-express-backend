1b22955847bf369d3c26fa0f669965016b96b98f
Noa <noa.benaziz@gmail.com>
2022-04-08 11:36:13 +0200

    routes fixed

--- routes/favorites.routes.js
+++ routes/favorites.routes.js
@@ -5,7 +5,7 @@ const Wishlist = require('../models/WishlistModel')
 
 router.get('/', async (req, res, next) => {
     let { countryId, userId } = req.params
-    const countryFav = await Wishlist.find().populate('country' )
+    const countryFav = await Wishlist.find({country: countryId, user: userId}).populate('country' )
   
     res.json(countryFav)
 })
